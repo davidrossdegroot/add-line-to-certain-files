5348e3b6bd5e9786e17ff71bce3f364e9b03c8f3
David De Groot <davidrossdegroot@gmail.com>
2015-08-12 21:36:18 -0400

    DDG: Got the script to add a new line with some dummy text.

--- add-nature.sh
+++ add-nature.sh
@@ -1,7 +1,24 @@
 #bin/bash
+# Utility to add some gradle nature that keeps being removed from our eclipse workspace
 
-for files in find . -name "project.project" -print
+# Test code that should be removed for the source - adds test data
+for i in `find . -name "project.project"` ; 
 do
-  echo Adding the gradle nature to the following files: $files
+  echo Removing $i
+  rm $i
+done
+
+cp template.project hello-test/project.project 
+cp template.project test1/project.project
+
+#source code 
+for i in `find . -name "project.project"` ; 
+ 
+do
+  echo Fixing $i;
+  #sed  -n '/from/p' $i
+  sed -i '/from/a HELLO WORLD' $i
+  cat $i
+  #sed  '\from>\a Hello World' $i
 done
 
