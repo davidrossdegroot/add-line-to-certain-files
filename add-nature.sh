61e65c25d3e77ad212c5330506a754dcb34c443c
David De Groot <davidrossdegroot@gmail.com>
2015-08-12 22:06:58 -0400

    DDG: Added futher documentation.

--- add-nature.sh
+++ add-nature.sh
@@ -1,11 +1,13 @@
 #bin/bash
 # Utility to add some gradle nature that keeps being removed from our eclipse workspace
 
-#source code 
+# Replace "project.project" with the name of the file you're searching for.
+# This will search the current directory recursively. 
 for i in `find . -name "project.project"` ; 
  
 do
   echo Fixing $i;
+  # Looks for a line with the patter "from" in it. Adds a line after the matching line with "HELLO WORLD".
   sed -i "/from/a HELLO WORLD" $i
 done
 
